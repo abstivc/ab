05a0cefd513ad57d3c44eff04ac21c149af4834a
abstivc <13162403691@163.com>
2019-03-18 17:58:45 +0800

    Spring Cloud Test

---
+++ eureka-server/src/main/java/com/song/leaf/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.song.leaf.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+		//new SpringApplicationBuilder(Application.class)
+		//		.web(true).run(args);
+	}
+
+}
---
+++ python-addition/read.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+with open('test.txt', 'r', encoding='utf-8') as f:
+    print(f.read())
